7280951fb8398e647a9fb59804cd8e4807448fdf
Jennifer Hu <jhu@LDI.INT>
2022-11-14 18:44:39 -0500

    Finished

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Select some elements...
+
+let header = document.querySelector('#page-header');
+header.style.textAlign = 'left';
+
+let dogImages = document.querySelectorAll('.dog-image');
+for (let i = 0; i<dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px';
+    // Flip the images so they are upside down.
+    dogImages[i].style.transform = 'scaleY(-1)';
+}
+
+
+// Make the dogs' names align to the left.
+let dogNames = document.querySelectorAll(".dog-name");
+for (let i = 0; i<dogNames.length; i++){
+    dogNames[i].style.textAlign = 'left';
+}
+
+// Change the color of the footer text.
+let footer = document.querySelector(".footer");
+footer.style.color = "yellow";
+footer.style.borderStyle = 'solid';
